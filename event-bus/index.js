e00660301679f6ff1e4ccd57c66b153dc645c099
Murali981 <muraliju981@gmail.com>
2025-01-06 22:54:45 +0530

    Completed the basic implementation of microservices with blog

--- event-bus/index.js
+++ event-bus/index.js
@@ -5,17 +5,33 @@ const axios = require("axios");
 const app = express();
 app.use(bodyParser.json());
 
+const events = []; // This array is going to store all the events that are comming from the event bus
+
 app.post("/events", (req, res) => {
   const event = req.body; // Whatever comes into the request body which is going to be our event, The event can be a JSON object,
   // It can be a string, It can be an array, It can be an object, It can be anything.
 
-  axios.post("http://localhost:4000/events", event); // on 4000 port our posts service is running
-  axios.post("http://localhost:4001/events", event); // on 4001 port our comments service is running
-  axios.post("http://localhost:4002/events", event); // on 4002 port our query service is running
-  axios.post("http://localhost:4003/events", event); // on 4003 port our moderation service is running
+  events.push(event); // The most recently occured event will be at the end of the events array
+
+  axios.post("http://localhost:4000/events", event).catch((err) => {
+    console.log(err.message);
+  }); // on 4000 port our posts service is running
+  axios.post("http://localhost:4001/events", event).catch((err) => {
+    console.log(err.message);
+  }); // on 4001 port our comments service is running
+  axios.post("http://localhost:4002/events", event).catch((err) => {
+    console.log(err.message);
+  }); // on 4002 port our query service is running
+  axios.post("http://localhost:4003/events", event).catch((err) => {
+    console.log(err.message);
+  }); // on 4003 port our moderation service is running
 
   res.send({ status: "OK" });
 });
+
+app.get("/events", (req, res) => {
+  res.send(events);
+}); // Whenever someone makes GET request to this endpoint, We are going to send back the events array
 
 app.listen(4005, () => {
   console.log("Listening on 4005");
--- query/index.js
+++ query/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
+const axios = require("axios");
+
 const app = express();
 
 app.use(bodyParser.json());
@@ -24,13 +26,7 @@ const posts = {}; // This posts object is to store all posts and comments
 
 //}
 
-app.get("/posts", (req, res) => {
-  res.send(posts); // We are sending the entire posts object
-});
-
-app.post("/events", (req, res) => {
-  const { type, data } = req.body;
-
+const handleEvent = (type, data) => {
   if (type === "PostCreated") {
     const { id, title } = data;
 
@@ -45,11 +41,54 @@ app.post("/events", (req, res) => {
     // array of comments for a particular post
   }
 
-  console.log(posts);
+  // We will watch for the CommentUpdated event then we are going to find the appropriate comment in our memory and then we are going to
+  // update the status to the status which is inside the event (or) which is received from the event bus
 
+  if (type === "CommentUpdated") {
+    const { id, content, postId, status } = data;
+    // As a quick remainder we are storing all the posts inside a posts object. We are going to find all the appropriate comments with
+    // the associated post and look at the array of comments and find the comment that we want to update
+
+    const post = posts[postId];
+    const comment = post.comments.find((comment) => {
+      return comment.id === id; // We are finding the comment that we want to update
+    });
+
+    comment.status = status;
+    comment.content = content;
+  }
+};
+
+app.get("/posts", (req, res) => {
+  res.send(posts); // We are sending the entire posts object
+});
+
+app.post("/events", (req, res) => {
+  // We are handling all the events that are comming here
+  const { type, data } = req.body;
+
+  handleEvent(type, data);
   res.send({});
 }); // This is going to receive events from the event bus
 
-app.listen(4002, () => {
+app.listen(4002, async () => {
   console.log("Listening on 4002");
+
+  // Whenever our query service comes online and it's listening on port 4002, Right after this it is a pretty good time to make a request
+  // over to our event bus and try to get the list of all the different events that have been emitted till this point of time.
+
+  try {
+    const res = await axios
+      .get("http://localhost:4005/events")
+      .catch((err) => {}); // This is going to return all the events that are occured during this point of
+    // time
+
+    for (let event of res.data) {
+      console.log("Processing the event:", event.type);
+
+      handleEvent(event.type, event.data);
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
 });
